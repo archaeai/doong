e490cdf58e766f753a1af68a26fb721327a1556d
정희주 <hizu@jeonghuijuui-MacBookAir.local>
2024-06-23 17:20:46 +0900

    test

--- src/components/AddCatModal.jsx
+++ src/components/AddCatModal.jsx
@@ -6,11 +6,18 @@ export default function AddCatModal({ closeModal }) {
   const [cat, setCat] = useState({ name: "", age: "", breed: "" });
   const { addCat } = useContext(CatContext);
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setCat((prevCat) => ({
+      ...prevCat,
+      [name]: value,
+    }));
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    new FormData();
-    // addCat(cat);
-    // closeModal();
+    addCat(cat);
+    closeModal();
   };
 
   return (
@@ -20,19 +27,41 @@ export default function AddCatModal({ closeModal }) {
           <h2>새 고양이 친구의 정보를 입력해주세요.</h2>
           <div>
             <label htmlFor="name">이름</label>
-            <input type="text" name="name" placeholder="이름을 입력하세요." />
+            <input
+              type="text"
+              name="name"
+              value={cat.name}
+              onChange={handleChange}
+              placeholder="이름을 입력하세요."
+            />
           </div>
           <div>
             <label htmlFor="breed">품종</label>
-            <input type="text" name="breed" placeholder="품종을 입력하세요." />
+            <input
+              type="text"
+              name="breed"
+              value={cat.breed}
+              onChange={handleChange}
+              placeholder="품종을 입력하세요."
+            />
           </div>
           <div>
-            <label htmlFor="">출생일</label>
-            <input type="date" name="breed" placeholder="품종을 입력하세요." />
+            <label htmlFor="birthDate">출생일</label>
+            <input
+              type="date"
+              name="birthDate"
+              value={cat.birthDate}
+              onChange={handleChange}
+            />
           </div>
           <div>
-            <label htmlFor="">입양일</label>
-            <input type="date" name="breed" placeholder="품종을 입력하세요." />
+            <label htmlFor="adoptDate">입양일</label>
+            <input
+              type="date"
+              name="adoptDate"
+              value={cat.adoptDate}
+              onChange={handleChange}
+            />
           </div>
           <button type="submit">저장</button>
           <button type="button" onClick={closeModal}>
