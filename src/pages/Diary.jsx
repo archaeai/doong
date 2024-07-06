0210a0502645e2ff3b1d1d988e0b390e8578b747
Heeju Jeong <hi22zu@naver.com>
2024-07-06 14:11:40 +0900

    일기페이지 레이아웃 설정

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -3,6 +3,8 @@ import useCurrentDate from "../hooks/useCurrentDate";
 
 import DiaryForm from "../components/DiaryForm";
 import Modal from "../UI/Modal";
+import diaryDefaultImg from "../assets/diary-default-image.png";
+import "../styles/Diary.css";
 
 export default function DiaryPage() {
   const { isModalOpen, openModal, closeModal } = useModal();
@@ -12,8 +14,26 @@ export default function DiaryPage() {
     <>
       <h1>{currentDate}</h1>
       <div className="page-content">
-        <h1>Diary Page</h1>
-        <button onClick={openModal}>일기를 추가해주세요</button>
+        <div className="diary-right-side">
+          <h2 className="add-diary-heading" onClick={openModal}>
+            일기쓰기
+          </h2>
+          <div className="diary-illustration-area">
+            <img
+              src={diaryDefaultImg}
+              alt="diary illustration"
+              className="diary-illustration"
+            />
+            <p>오늘의 둥이</p>
+          </div>
+        </div>
+        <div className="divider"></div>
+        <div className="diary-text-area">
+          <p className="no-diary-message">아직 작성된 일기가 없습니다.</p>
+          <br />
+          <p>오늘의 일기를 작성해 보세요!</p>
+        </div>
+
         <Modal isOpen={isModalOpen} onClose={closeModal}>
           <DiaryForm closeModal={closeModal} />
         </Modal>
