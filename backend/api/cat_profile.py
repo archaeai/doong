01ca71ac5ade6e811d219ceba7560b0ee3450928
japy <108922376+archaeai@users.noreply.github.com>
2024-07-14 11:24:22 +0900

    표정추가

--- backend/api/cat_profile.py
+++ backend/api/cat_profile.py
@@ -43,10 +43,10 @@ async def create_cat_profile(
         weight: float,
         photo: UploadFile = File(None),
         db: Session = Depends(get_db),
-        #current_user: str = Depends(get_current_user)
+        current_user: str = Depends(get_current_user)
 ):
-    #UPLOAD_DIR = f"uploads/{current_user}/"
-    UPLOAD_DIR = f"uploads/"
+    UPLOAD_DIR = f"uploads/{current_user}/"
+    # UPLOAD_DIR = f"uploads/"
 
     # Ensure the upload directory exists
     if not os.path.exists(UPLOAD_DIR):
@@ -59,8 +59,10 @@ async def create_cat_profile(
         with open(file_path, "wb") as buffer:
             copyfileobj(photo.file, buffer)
         photo_url = file_path
-    print('*******')
-    print(f"{birthday}, {neutered}, {type(neutered)}")
+        
+        # cat pace 추가
+        crud_cat_profile.create_cat_face(UPLOAD_DIR, filename)
+        
 
     cat_profile = CatProfileCreate(
         name=name,
@@ -73,8 +75,7 @@ async def create_cat_profile(
         litter_date=litter_date,
         neutered=neutered,
         weight=weight,
-        #user_id=current_user,
-        user_id='string',
+        user_id=current_user,
         photo_url=photo_url
     )
     return crud_cat_profile.create_cat_profile(db=db, cat_profile=cat_profile)
--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,6 @@
 class Settings:
     SECRET_KEY: str = "your_secret_key"
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 10000
 
 settings = Settings()
