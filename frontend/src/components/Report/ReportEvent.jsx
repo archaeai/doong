3876f111e682d40f1af25a14f5152290d7ac4821
japy <108922376+archaeai@users.noreply.github.com>
2024-07-25 17:06:45 +0900

    date, note 배열 순회하는 부분 코드 수정

--- frontend/src/components/Report/ReportEvent.jsx
+++ frontend/src/components/Report/ReportEvent.jsx
@@ -5,14 +5,14 @@ export default function ReportEvent({ report }) {
     );
   }
 
-  const specialNotes = Object.entries(report.special_notes);
+  const specialNotes = report.special_notes;
   const generateKey = (date, note) => `${date}-${note}`;
 
   return (
     <div className="report-note">
       <h4 className="report-heading">특이사항</h4>
       <ul className="report-note__ul">
-        {specialNotes.map(([date, note]) => (
+        {specialNotes.map(({ date, note }) => (
           <li key={generateKey(date, note)} className="report-note__li">
             <span className="report-note__li-date">{date}: </span>
             <span>{note}</span>
